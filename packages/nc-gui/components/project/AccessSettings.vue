11bfdc5fb6881a874675305c47caab8bc3c99cda
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-08-01 11:50:59 +0000

    chore(nc-gui): lint

--- packages/nc-gui/components/project/AccessSettings.vue
+++ packages/nc-gui/components/project/AccessSettings.vue
@@ -229,7 +229,13 @@ watch(currentBase, () => {
     </div>
     <template v-else>
       <div class="w-full flex flex-row justify-between items-center max-w-350 mt-6 mb-2 px-1">
-        <a-input v-model:value="userSearchText"  type="text" :placeholder="$t('title.searchMembers')" class="!max-w-90 nc-input-sm"  allow-clear>
+        <a-input
+          v-model:value="userSearchText"
+          type="text"
+          :placeholder="$t('title.searchMembers')"
+          class="!max-w-90 nc-input-sm"
+          allow-clear
+        >
           <template #prefix>
             <GeneralIcon icon="search" class="mr-2 h-4 w-4 text-gray-500" />
           </template>
